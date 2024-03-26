188e093cec2b64f477d59ced56235a006e8d722a
Polonnikova Anna <xxxuko@gmail.com>
2024-03-26 19:31:05 +0200

    ceate models for planetarium app

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=50)
+    astronomy_shows = models.ManyToManyField(
+        AstronomyShow,
+        on_delete=models.DO_NOTHING,
+        related_name="show_themes",
+    )
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(
+        AstronomyShow,
+        on_delete=models.CASCADE,
+        related_name="show_sessions"
+    )
+    planetarium_dome = models.ForeignKey(
+        PlanetariumDome,
+        on_delete=models.CASCADE,
+        related_name="show_sessions"
+    )
+    show_time = models.DateTimeField()
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reservations"
+    )
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(
+        ShowSession,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
