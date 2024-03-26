955cf784ecbf17bec2ce3f8d964783da179cd6be
Polonnikova Anna <xxxuko@gmail.com>
2024-03-26 20:18:00 +0200

    register models in admin panne;

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    AstronomyShow,
+    ShowTheme,
+    PlanetariumDome,
+    ShowSession,
+    Reservation,
+    Ticket,
+)
+
+app_name = "planetarium"
+
+admin.site.register(AstronomyShow)
+admin.site.register(ShowTheme)
+admin.site.register(PlanetariumDome)
+admin.site.register(ShowSession)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
