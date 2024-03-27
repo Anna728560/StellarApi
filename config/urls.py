dd3502d62d0cab39c508ef2cd2ea056902e57344
Polonnikova Anna <xxxuko@gmail.com>
2024-03-27 17:57:27 +0200

    create token serializer

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/planetarium/", include("planetarium.urls", namespace="planetarium")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
